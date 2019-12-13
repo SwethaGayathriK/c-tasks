5608d2b9a123ac2ebb10cdcf806da33a86557990
SwethaGayathriK <55136622+SwethaGayathriK@users.noreply.github.com>
2019-12-13 11:16:07 +0530

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int n;
+	cout<<"enter a value:"<<endl;
+	cin>>n;
+	if(n%2==0)
+	cout<<"the number is even.";
+	else
+	cout<<"the number is odd.";
+	
+}
